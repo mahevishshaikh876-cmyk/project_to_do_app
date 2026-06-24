ba8d70ca279048892a13f595fbcdd5943f2a2f12
mahi_shaikh. <mahevishshaikh876@gmail.com>
2026-06-24 13:13:02 +0530

    main.py

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
-from flask import Flask
-from flask import render_template
-app=Flask(__name__,template_folder="template", static_folder="static")#"__main__"#print(__name_)
+from flask import Flask, request,render_template
 
+app = Flask(__name__, template_folder="template", static_folder="static")
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-
-@app.route("/login")
+@app.route("/login", methods=["POST"])
 def login():
-    return render_template("login.html")
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    return f"""
+    Email : {email}
+    Password : {password}
+    """
 
 @app.route("/dashb")
 def dashboard():
     return render_template("dashboard.html")
 
-@app.route("/reg")
-def register():
-    return render_template("register.html")
 
+@app.route("/reg", methods=["POST"])
+def register():
+
+    fullname = request.form.get("fullname")
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    return f"""
+    Name : {fullname}
+    Email : {email}
+    Password : {password}
+    """
 
 if __name__=="__main__":
     app.run(debug=True)
